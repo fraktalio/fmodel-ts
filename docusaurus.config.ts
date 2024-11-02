f04bbcc1267f441677089cdf61e91fb64217e919
Ivan Dugalic <idugalic@gmail.com>
2024-11-02 01:25:01 +0100

    removed edit links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -48,8 +48,8 @@ const config: Config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
